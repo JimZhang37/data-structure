dfbed63ebd35541a0fc6017fe4d143e783f246f7
ZHANG Yaohua <zhangyaohua40605@gmail.com>
2020-03-02 13:07:28 +0800

    correct mistakes in problem 6

--- explanation_6.md
+++ explanation_6.md
@@ -1,9 +1,30 @@
 # Why I choose this data structure
-I use python's set in this project
+- union function
+
+
+In union function, the first step is to connect 2 lists together by assigning last item in the first list a new next value.
+This new next value is the head of the second list. This operation has time complexity of O(n) which n is the length of the first list.
+
+In the second step, I use python's set in this project so I can have constant search time and handle duplicates.
+I use a for loop to go through all items to see if it has appeared before. If it is the first appearance, I put it in a set.
+If it is not the first appearance, I would delete it.
+
+I adopted double linked list so I have O(1) time complexity to remove an item from the list.
+
+- intersection
+
+In the first design, my time complexity is O(n**2) as I have 2 nested `for loop` that can compare every item in list 1 with every item in list 2.
+To improve its performance, in the inner for loop, I will delete the item in the second list when I found it has the same value of a item from another list.
+
+Another improvement is I put the shorter list in the outer loop and longer list in the inner loop.
+
 
 # Time Complexity
 
-The time complexity is O(n)
-I used for loop to add items in lists to the set, so all inputs will be visited once.
-The union and intersection operation of set have O(min(len(set1), len(set2))).
-So in total, the time complexity is still O(n)
+The time complexity is O(n) for union and O(n * log(n)) in the intersection function. 
+In the worst case, if list 1 and list 2 have nothing in common, the time complexity for
+intersection will be O(n**2) because the inner loop's list will never be deceasing.
+
+# Space 
+I use deep copy to copy inputs to a new space, so that the original lists will not be tampered. I use inplace algorithm in both union
+and intersection. So the space complexity for both function is O(n)
--- problem_6.py
+++ problem_6.py
@@ -1,7 +1,10 @@
+import copy
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        self.prev = None
 
     def __repr__(self):
         return str(self.value)
@@ -13,10 +16,11 @@ class LinkedList:
 
     def __str__(self):
         cur_head = self.head
-        out_string = ""
+        out_string = "{"
         while cur_head:
             out_string += str(cur_head.value) + " -> "
             cur_head = cur_head.next
+        out_string += "}"
         return out_string
 
     def append(self, value):
@@ -27,8 +31,9 @@ class LinkedList:
         node = self.head
         while node.next:
             node = node.next
-
-        node.next = Node(value)
+        new_node = Node(value)
+        node.next = new_node
+        new_node.prev =node
 
     def size(self):
         size = 0
@@ -42,46 +47,80 @@ class LinkedList:
 
 def union(llist_1, llist_2):
     # Your Solution Here
-    u = set()
-    current = llist_1.head
-    if current:
-        u.add(current.value)
-        while current.next:
-            current = current.next
-            u.add(current.value)
+    llist_1 = copy.deepcopy(llist_1)
+    llist_2 = copy.deepcopy(llist_2)
+    if llist_1.head is None:
+        return setlist(llist_2)
+    else:
+        node = llist_1.head
+        while node.next:
+            node = node.next
+        node.next = llist_2.head
+        return setlist(llist_1)
 
-    current = llist_2.head
-    if current:
-        u.add(current.value)
-        while current.next:
-            current = current.next
-            u.add(current.value)
-    return u
+
+def setlist(llist):
+
+    if llist.head is None:
+        return llist
+    else:
+        s = set()
+        node = llist.head
+        s.add(node.value)
+        next = node.next
+        while next:
+            if next.value not in s:
+                s.add(next.value)
+                node = next
+                next = next.next
+            else:
+                node.next = next.next
+                next = next.next
+        return llist
 
 
 def intersection(llist_1, llist_2):
     # Your Solution Here
-    u1 = set()
-    current = llist_1.head
-    if current:
-        u1.add(current.value)
-        while current.next:
-            current = current.next
-            u1.add(current.value)
+    list3 = LinkedList()
+    if (llist_2.size() == 0) or (llist_1.size() == 0) :
+        return list3
 
-    u2 = set()
-    current = llist_2.head
-    if current:
-        u2.add(current.value)
-        while current.next:
-            current = current.next
-            u2.add(current.value)
+    llist_1 = copy.deepcopy(llist_1)
+    llist_2 = copy.deepcopy(llist_2)
+    llist_1 = setlist(llist_1)
+    llist_2 = setlist(llist_2)
 
-    return u1 & u2
+    size1 = llist_1.size()
+    size2 = llist_2.size()
+    if size1 > size2:
+        newlist = llist_1
+        llist_1 = llist_2
+        llist_2 = newlist
+
+    node = llist_1.head
+    while node:
+        node2 = llist_2.head
+        while node2:
+            if node.value == node2.value:
+                list3.append(node.value)
+                if node2 is llist_2.head:
+                    llist_2.head = node2.next
+                else:
+                    node2.prev.next = node2.next
+                    if node2.next is not None:
+                        node2.next.prev = node2.prev
+                break
+            else:
+                node2 = node2.next
+
+        node = node.next
+
+
+    return list3
 
 
 # Test case 1
-
+print("test 1")
 linked_list_1 = LinkedList()
 linked_list_2 = LinkedList()
 
@@ -95,10 +134,11 @@ for i in element_2:
     linked_list_2.append(i)
 
 print(union(linked_list_1,linked_list_2)) #expect {32, 65, 2, 35, 3, 4, 6, 1, 9, 11, 21}
+
 print(intersection(linked_list_1,linked_list_2)) # expect {4, 21, 6}
 
 # Test case 2
-
+print("test2")
 linked_list_3 = LinkedList()
 linked_list_4 = LinkedList()
 
@@ -115,11 +155,11 @@ print(union(linked_list_3,linked_list_4)) # expect {65, 2, 35, 3, 4, 6, 1, 7, 8,
 print(intersection(linked_list_3,linked_list_4)) # expect {}
 
 # Test case 3
-
+print("test 3")
 linked_list_3 = LinkedList()
 linked_list_4 = LinkedList()
 
-element_1 = []
+element_1 = [2, 2]
 element_2 = [1]
 
 for i in element_1:
@@ -128,5 +168,5 @@ for i in element_1:
 for i in element_2:
     linked_list_4.append(i)
 
-print(union(linked_list_3,linked_list_4)) # expect {1}
+print(union(linked_list_3,linked_list_4)) # expect {1, 2}
 print(intersection(linked_list_3,linked_list_4)) # expect {}
